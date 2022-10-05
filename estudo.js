bdd0ffc6cc16250d86b4fe950ea6aaddbb9b8697
felipedxtt <110242246+felipedxtt@users.noreply.github.com>
2022-10-05 08:18:57 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -3,16 +3,31 @@ let nome = "Felipe"
 
 function escreva(){
     document.write("Tabuada do " + "<br>")
-    document.write(tabuada + " x 1 ="+(tabuada*1)+"<br>");
-    document.write(tabuada + " x 2 ="+(tabuada*2)+"<br>");
-    document.write(tabuada + " x 3 ="+(tabuada*3)+"<br>");
-    document.write(tabuada + " x 4 ="+(tabuada*4)+"<br>");
-    document.write(tabuada + " x 5 ="+(tabuada*5)+"<br>");
-    document.write(tabuada + " x 6 ="+(tabuada*6)+"<br>");
-    document.write(tabuada + " x 7 ="+(tabuada*7)+"<br>");
-    document.write(tabuada + " x 8 ="+(tabuada*8)+"<br>");
-    document.write(tabuada + " x 9 ="+(tabuada*9)+"<br>");
-    document.write(tabuada + " x 10 ="+(tabuada*10)+"<br>");
+    document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
+    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
+    document.write(tabuada + " x 4 = "+(tabuada*4)+"<br>");
+    document.write(tabuada + " x 5 = "+(tabuada*5)+"<br>");
+    document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
+    document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
+    document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
+    document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
+    document.write(tabuada + " x 10 = "+(tabuada*10)+"<br>");
     document.write("Feito por " + nome);
+}
 
+function minhaTabuada(){
+    for(let i = 2; i< 9; i++){
+        document.write("Tabuada do " + i + "<br>");
+        for(let j = 1; j <= 10; j++){
+            document.write(i + " x " + j + " = "+(j*i)+"<br>");
+        }
+        document.write("<br>");
+    }
+
+    
+    function quadrado(){
+        for(let i = 2; i <=20; i++){
+            document.write("O quadrado de " + i + "é" + (i+i)+ "<br>");
+    }
 }
